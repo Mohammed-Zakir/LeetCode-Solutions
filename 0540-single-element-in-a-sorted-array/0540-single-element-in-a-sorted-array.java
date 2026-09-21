a290bd0923f518134b43ef3baf495477fd5c13a1
Mohammed-Zakir <zakirmohd2211@gmail.com>
2026-09-21 15:27:07 +0530

    Time: 0 ms (100%), Space: 52.7 MB (76.51%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,29 +1,50 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+    public int singleNonDuplicate(int[] arr) {
 
-        int left = 0;
-        int right = nums.length - 1;
+        int n = arr.length;
 
-        while (left < right) {
+        // Only one element
+        if (n == 1) {
+            return arr[0];
+        }
 
-            int mid = left + (right - left) / 2;
+        // Check first element
+        if (arr[0] != arr[1]) {
+            return arr[0];
+        }
 
-            // Make mid even
-            if (mid % 2 == 1) {
-                mid--;
+        // Check last element
+        if (arr[n - 1] != arr[n - 2]) {
+            return arr[n - 1];
+        }
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            // Single element found
+            if (arr[mid] != arr[mid + 1] &&
+                arr[mid] != arr[mid - 1]) {
+
+                return arr[mid];
             }
 
-            // Correct pair → single element is on the right
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
+            // We are in the LEFT half
+            if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) ||
+                (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+
+                low = mid + 1;
             }
 
-            // Broken pair → single element is at mid or on the left
+            // We are in the RIGHT half
             else {
-                right = mid;
+                high = mid - 1;
             }
         }
 
-        return nums[left];
+        return -1;
     }
 }
